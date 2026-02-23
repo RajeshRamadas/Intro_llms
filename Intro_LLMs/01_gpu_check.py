01a4cafe1775244708a73d383450812c26997605
R Rajesh Kumar (uie65064) <rajesh.kumar.r@aumovio.com>
2026-02-23 09:16:15 +0530

    training

--- Intro_LLMs/01_gpu_check.py
+++ Intro_LLMs/01_gpu_check.py
@@ -1,5 +1,9 @@
 
 import torch
+#import torchtext
+
+print("Torch:", torch.__version__)
+#print("Torchtext:", torchtext.__version__)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
@@ -8,10 +12,10 @@ print(torch.cuda.is_available())
 print(torch.cuda.device_count())
 print(torch.cuda.get_device_name(0) if torch.cuda.is_available() else "No CUDA device")
 
-
 print("CUDA available:", torch.cuda.is_available())
 print("Torch CUDA version:", torch.version.cuda)
 
 if torch.cuda.is_available():
     print("GPU:", torch.cuda.get_device_name(0))
     print("VRAM (GB):", torch.cuda.get_device_properties(0).total_memory / 1e9)
+
